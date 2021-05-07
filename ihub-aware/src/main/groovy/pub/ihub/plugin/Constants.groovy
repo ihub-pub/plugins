34c7c2efbe92d212a9623eca8dfdfae84d5adce2
henry <henry.git@outlook.com>
2021-05-07 17:22:38 +0800

    ✏️ 清除多余的组件管理

--- ihub-aware/src/main/groovy/pub/ihub/plugin/Constants.groovy
+++ ihub-aware/src/main/groovy/pub/ihub/plugin/Constants.groovy
@@ -32,16 +32,15 @@ final class Constants {
 	static final String GROOVY_VERSION = '3.0.8'
 
 	static final List<Tuple3<String, String, String>> GROUP_MAVEN_BOM_VERSION_CONFIG = [
-//		of('pub.ihub.lib'					, 'ihub-libs'							, '1.0.0-SNAPSHOT'		), TODO 由于GitHub仓库token只能个人使用，组件发布到中央仓库方可使用
-		of(GROOVY_GROUP_ID						, 'groovy-bom'							, GROOVY_VERSION			),
-		of('org.spockframework'				, 'spock-bom'							, '2.0-M5-groovy-3.0'	),
-		of('org.springframework.boot'		, 'spring-boot-dependencies'			, '2.4.5'				),
-		of('org.springframework.cloud'		, 'spring-cloud-dependencies'			, '2020.0.2'			),
-		of('org.springframework.security'	, 'spring-security-bom'					, '5.4.6'				),
-		of('com.alibaba.cloud'				, 'spring-cloud-alibaba-dependencies'	, '2021.1'				),
+//		of('pub.ihub.lib'				, 'ihub-libs'							, '1.0.0-SNAPSHOT'		), TODO 由于GitHub仓库token只能个人使用，组件发布到中央仓库方可使用
+		of(GROOVY_GROUP_ID					, 'groovy-bom'							, GROOVY_VERSION			),
+		of('org.spockframework'			, 'spock-bom'							, '2.0-M5-groovy-3.0'	),
+		of('org.springframework.boot'	, 'spring-boot-dependencies'			, '2.4.5'				),
+		of('org.springframework.cloud'	, 'spring-cloud-dependencies'			, '2020.0.2'			),
+		of('com.alibaba.cloud'			, 'spring-cloud-alibaba-dependencies'	, '2021.1'				),
 		of('com.github.xiaoymin'			, 'knife4j-dependencies'				, '3.0.2'				),
-		of('com.sun.xml.bind'				, 'jaxb-bom-ext'						, '3.0.1'				),
-		of('de.codecentric'					, 'spring-boot-admin-dependencies'		, '2.4.1'				),
+		of('com.sun.xml.bind'			, 'jaxb-bom-ext'						, '3.0.1'				),
+		of('de.codecentric'				, 'spring-boot-admin-dependencies'		, '2.4.1'				),
 	]
 
 	static final Map<String, String> GROUP_MAVEN_VERSION_CONFIG = [
@@ -51,11 +50,9 @@ final class Constants {
 	@SuppressWarnings('DuplicateStringLiteral')
 	static final List<Tuple3<String, String, List<String>>> GROUP_DEPENDENCY_VERSION_CONFIG = [
 		of(GROOVY_GROUP_ID				, GROOVY_VERSION	, ['groovy-all']),
-		of('org.projectlombok'		, '1.18.20'		, ['lombok']),
 		of('com.alibaba'			, '1.2.76'		, ['fastjson']),
 		of('com.alibaba'			, '1.2.6'		, ['druid', 'druid-spring-boot-starter']),
 		of('com.alibaba.p3c'		, '2.1.1'		, ['p3c-pmd']),
-		of('javax.xml.bind'			, '2.3.1'		, ['jaxb-api']),
 		of('com.baomidou'			, '3.4.2'		, ['mybatis-plus', 'mybatis-plus-boot-starter', 'mybatis-plus-generator']),
 		of('com.github.xiaoymin'	, '2.0.8'		, ['knife4j-aggregation-spring-boot-starter']),
 		of('com.athaydes'			, '2.0.1-RC3'	, ['spock-reports']),
