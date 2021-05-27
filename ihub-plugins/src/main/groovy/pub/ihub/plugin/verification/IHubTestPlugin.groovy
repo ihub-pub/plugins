247408ee591e8c1b77a93cb14cba5d6682b39322
henry <henry.git@outlook.com>
2021-05-27 23:11:01 +0800

    🐛 Task配置系统属性没有配置会报空指针错误 #30

--- ihub-plugins/src/main/groovy/pub/ihub/plugin/verification/IHubTestPlugin.groovy
+++ ihub-plugins/src/main/groovy/pub/ihub/plugin/verification/IHubTestPlugin.groovy
@@ -18,6 +18,7 @@ package pub.ihub.plugin.verification
 import static pub.ihub.plugin.IHubPluginAware.EvaluateStage.AFTER
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.GroovyPlugin
 import org.gradle.api.tasks.testing.Test
 import pub.ihub.plugin.IHubPluginAware
 import pub.ihub.plugin.bom.IHubBomExtension
@@ -31,15 +32,21 @@ class IHubTestPlugin implements IHubPluginAware<IHubTestExtension> {
 	@Override
 	void apply(Project project) {
 		getExtension(project, IHubBomExtension) {
-			it.importBoms {
-				group 'org.spockframework' module 'spock-bom' version '2.0-M4-groovy-3.0'
-			}
-			it.dependencyVersions {
-				group 'com.athaydes' modules 'spock-reports' version '2.0.1-RC3'
-			}
-			it.dependencies {
-				testImplementation 'org.spockframework:spock-spring'
-				testRuntimeOnly 'com.athaydes:spock-reports'
+			if (project.plugins.hasPlugin(GroovyPlugin)) {
+				it.importBoms {
+					group 'org.spockframework' module 'spock-bom' version '2.0-M4-groovy-3.0'
+				}
+				it.dependencyVersions {
+					group 'com.athaydes' modules 'spock-reports' version '2.0.1-RC3'
+				}
+				it.dependencies {
+					testImplementation 'org.spockframework:spock-spring'
+					testRuntimeOnly 'com.athaydes:spock-reports'
+				}
+			} else {
+				it.dependencies {
+					testImplementation 'org.junit.jupiter:junit-jupiter'
+				}
 			}
 		}
 		createExtension(project, 'iHubTest', IHubTestExtension, AFTER) { ext ->
