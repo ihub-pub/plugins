221ed66dc2f4268453b366529d6d89c8cfea2a2d
henry <henry.git@outlook.com>
2024-04-25 01:28:53 +0800

    fix(shadow): 修复ihub-shadow组件发布签名错误
    Signed-off-by: henry <henry.git@outlook.com>

--- ihub-shadow/src/test/groovy/pub/ihub/plugin/shadow/IHubShadowPluginTest.groovy
+++ ihub-shadow/src/test/groovy/pub/ihub/plugin/shadow/IHubShadowPluginTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 the original author or authors.
+ * Copyright (c) 2023-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,6 +70,7 @@ class IHubShadowPluginTest extends IHubSpecification {
         buildFile << '''
             apply {
                 plugin 'maven-publish'
+                plugin 'signing'
                 plugin 'pub.ihub.plugin.ihub-shadow'
             }
         '''
