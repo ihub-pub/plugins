492da1af3d39b99c8e8c609cfaf245810f2c6be8
Henry <henry.git@outlook.com>
2025-08-10 18:18:46 +0800

    build(deps): Update gradle to 9.0.0

--- ihub-base/src/test/groovy/pub/ihub/plugin/IHubPrintPlugin.groovy
+++ ihub-base/src/test/groovy/pub/ihub/plugin/IHubPrintPlugin.groovy
@@ -39,6 +39,9 @@ class IHubPrintPlugin extends IHubProjectPluginAware<IHubPrintExtension> {
         printLineConfigContent 'test', [
             '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'
         ], 't'
+        printLineConfigContent 'title1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', [
+            '123'
+        ]
         printMapConfigContent 'test', 'ID', 'Version', [a: '1', b: '2', c: ['3', '4']]
         registerTask 'printConfig', {
             printConfigContent 'task', []
